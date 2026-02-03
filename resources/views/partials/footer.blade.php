90e4936ab37748a9cee2061c66cdd6d758c811e2
rntalas <rafailntalas@outlook.com>
2026-02-03 13:11:58 +0200

    feat: stylize header & footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
-<div class="fixed bottom-0 flex justify-center items-center bg-zinc-100 w-full py-2 lg:py-4">
+<div class="fixed bottom-0 flex justify-center items-center bg-zinc-100 w-full py-2 lg:py-4 border-t border-zinc-200">
     Copyright &copy; Rafail Ntalas {{ date('Y') }}
 </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-    <header class="flex justify-between items-center px-8 py-4 bg-zinc-100">
+    <header class="sticky top-0 flex justify-between items-center px-8 py-4 bg-zinc-100 border-b border-zinc-200 shadow-xl z-10 mb-6">
         <div class="flex justify-center items-center gap-5 lg:gap-12">
             <div class="h-8 w-8 cursor-pointer">
                 @svg('heroicon-o-bars-3')
@@ -18,15 +18,13 @@
             <a href="/">
                 <img src="{{ asset('images/logo.png') }}" alt="logo" class="h-5 md:h-10 lg:h-15 w-auto">
             </a>
-
-            <a href="/" class="btn">Lessons</a>
         </div>
 
         <div class="flex justify-center items-center gap-4 lg:mr-20">
             <input type="text" placeholder="Search..."
-                class="hidden md:block md:w-64 lg:w-96 xl:w-120 border rounded-full px-4 py-2" name="lesson">
+                class="hidden md:block md:w-64 lg:w-96 xl:w-120 border border-zinc-200 rounded-full px-4 py-2" name="lesson">
 
-            <div class="w-10 h-10 rounded-full p-2 border bg-white cursor-pointer hover:shadow-md">
+            <div class="w-10 h-10 rounded-full p-2 border border-zinc-200 bg-white cursor-pointer hover:shadow-md">
                 @svg('heroicon-o-magnifying-glass')
             </div>
         </div>
