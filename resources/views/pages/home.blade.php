9a717e93ccbaba2139b6b2775cdd862330397193
rntalas <rafailntalas@outlook.com>
2026-02-03 14:26:39 +0200

    feat: stylize header & home, create add-subject page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,23 @@
 <x-layout :title="$title">
-    <div class="text-black">
+    <div
+        class="flex justify-center mx-auto border border-zinc-400 h-180 w-135 rounded-xl bg-white p-8 shadow-xl relative">
+        <div class="flex flex-col flex-1">
+            <p class="mb-4">
+                <span class="text-xl mb-4">Welcome to <a href="/" class="font-bold hover:underline">Answer
+                        Pod</a>!</span><br>
+                <span class="text-lg">The best place to store textbook materials for your school subjects.</span>
+            </p>
+            @if ($subjects->count() > 0)
+            @else
+                <p class="text-lg">
+                    No subjects were found. To create a subject click <a href="/add-subject"
+                        class="underline hover:no-underline inline-block">here</a>.
+                </p>
+            @endif
+        </div>
 
+        <div class="absolute bottom-0 mb-6">
+            <img src="{{ asset('images/logo.png') }}" alt="logo" class="h-5 lg:h-10 w-auto">
+        </div>
     </div>
 </x-layout>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,11 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title }}</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
     @vite('resources/js/app.js')
 </head>
 
 <body>
-    <header class="sticky top-0 flex justify-between items-center px-8 py-4 bg-zinc-100 border-b border-zinc-200 shadow-xl z-10 mb-6">
+    <header
+        class="sticky top-0 flex justify-between items-center px-8 py-4 bg-zinc-100 border-b border-zinc-200 shadow-xl z-10 mb-8">
         <div class="flex justify-center items-center gap-5 lg:gap-12">
             <div class="h-8 w-8 cursor-pointer">
                 @svg('heroicon-o-bars-3')
@@ -21,8 +23,8 @@
         </div>
 
         <div class="flex justify-center items-center gap-4 lg:mr-20">
-            <input type="text" placeholder="Search..."
-                class="hidden md:block md:w-64 lg:w-96 xl:w-120 border border-zinc-200 rounded-full px-4 py-2" name="lesson">
+            <input type="text" placeholder="Search..." class="hidden md:block md:w-48 xl:w-96 rounded-full bg-white!"
+                name="lesson">
 
             <div class="w-10 h-10 rounded-full p-2 border border-zinc-200 bg-white cursor-pointer hover:shadow-md">
                 @svg('heroicon-o-magnifying-glass')
