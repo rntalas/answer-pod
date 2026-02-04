f79107b063970ba662c66e80d28310f2027b3adf
rntalas <rafailntalas@outlook.com>
2026-02-05 01:05:04 +0200

    feat: update app.php

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -1,12 +1,25 @@
 <?php
 
 return [
+    'title' => 'Welcome!',
     'welcome' => 'Welcome to <a href="/" class="font-bold hover:underline">Answer Pod</a>!',
     'description' => 'The best place to store textbook materials for your school subjects.',
     'subjects' => [
         'found' => 'The following subjects were found:',
         '!found' => 'No subjects were found.',
-        'create' => 'To create a subject click <a href="subjects/create" class="link">here</a>.',
+        'create' => 'To create a subject click <a href="/subject/add" class="link">here</a>.',
     ],
-    'search' => 'Search...',
+    'placeholder' => [
+        'search' => 'Search…',
+    ],
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'cancel' => 'Cancel',
+    'action-undone' => 'This action cannot be undone.',
+    'error' => [
+        '404' => [
+            'title' => 'Page not found!',
+            'description' => 'We are sorry but the page you searched for wasn\'t found. To return to home click <a href="/" class="link">here</a>.',
+        ],
+    ],
 ];
