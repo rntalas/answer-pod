a98fcf83499a04456b2b03c1eae8f75d6ce37b8f
rntalas <rafailntalas@outlook.com>
2026-02-05 01:20:18 +0200

    feat: update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,7 @@
 @include('partials.header')
 <main>
-    {{ $slot }}
+    <div class="flex flex-col mx-auto border border-zinc-400 min-h-180 w-135 rounded-xl bg-white p-8 shadow-xl">
+        {{ $slot }}
+    </div>
 </main>
 @include('partials.footer')
