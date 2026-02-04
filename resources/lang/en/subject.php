0d9a45764454d65c7f886c721346a90465ed516b
rntalas <rafailntalas@outlook.com>
2026-02-05 01:24:17 +0200

    feat(lang): create subject.php

---
+++ resources/lang/en/subject.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'create' => 'Create a subject',
+    'units' => 'Number of units: :count.',
+    'entries' => 'You can see the entries per unit on the side menu. Otherwise, to create a new entry you can click <a href="entries/create" class="link">here</a>.',
+    'button' => [
+        'add' => 'Add subject',
+        'edit' => 'Edit subject',
+    ],
+    'label' => [
+        'name' => 'Name:',
+        'units' => 'Number of units:',
+    ],
+    'placeholder' => [
+        'name' => 'Subject',
+    ],
+    'error' => [
+        'name' => [
+            'required' => 'Please enter a name.',
+            'exists' => 'This name already exists.',
+        ],
+    ],
+    'confirm' => 'Do you want to delete this subject?',
+];
