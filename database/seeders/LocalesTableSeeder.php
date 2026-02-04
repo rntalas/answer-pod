6f7c6a7a261f0c00d52c5e0c80dc409034b4ef6d
rntalas <rafailntalas@outlook.com>
2026-02-04 02:07:43 +0200

    chore: create seeder for Locales

---
+++ database/seeders/LocalesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LocalesTableSeeder extends Seeder
+{
+    public function run():void {
+        DB::table('locales')->insert([
+           ['name' => 'English', 'code' => 'en', 'image' => 'images/lang/en.png', 'created_at' => now(), 'updated_at' => now()],
+           ['name' => 'Greek', 'code' => 'el', 'image' => 'images/lang/el.png', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
