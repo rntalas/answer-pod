fd0743d40c38f3d4e74362d613bad2e33fca9b33
rntalas <rafailntalas@outlook.com>
2026-02-05 19:15:48 +0200

    feat(entry): add lang files

---
+++ resources/lang/en/entry.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'create' => 'Create an entry',
+    'hint' => [
+        'statement' => 'You can enter a statement or upload an image by clicking on the image above.',
+    ],
+    'button' => [
+        'add' => 'Add entry',
+        'save' => 'Save changes',
+    ],
+    'label' => [
+        'number' => 'Number:',
+        'subject' => 'Subject:',
+        'unit' => 'Unit:',
+        'statement' => 'Statement:',
+        'solution' => 'Solution:',
+        'locale' => 'Language:',
+    ],
+    'placeholder' => [
+        'statement' => 'Statement',
+        'solution' => 'Solution',
+    ],
+    'error' => [
+        'statement' => 'Please enter a statement.',
+        'solution' => 'Please enter a solution.',
+    ],
+    'confirm' => 'Do you want to delete this entry?',
+];
