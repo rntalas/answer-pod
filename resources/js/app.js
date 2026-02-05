5a5d3ee84105d2c05e8feadf4d32e62851bbb610
rntalas <rafailntalas@outlook.com>
2026-02-05 14:55:48 +0200

    feat(js/app.js): simplify localeSwitcher function

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,28 +4,20 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
-const mapLocales = (locales) => {
-    return locales.map(locale => ({
-        id: locale.id,
-        code: locale.code,
-        name: locale.name,
-        image: locale.image?.startsWith('http') ? locale.image : `/${locale.image}`,
-    }));
-}
-
 Alpine.data('localeSwitcher', (locales) => ({
     open: false,
-    locales: mapLocales(locales),
+    locales: locales.map(locale => ({
+        ...locale,
+        image: locale.image?.startsWith('http') ? locale.image : `/${locale.image}`,
+    })),
 
     toggle() {
         this.open = !this.open;
     },
 
-    setLocale(locale) {
-        const path = window.location.pathname === '/'
-            ? ''
-            : window.location.pathname;
-        window.location.href = `${path}?lang=${locale.code}`;
+    setLocale({ code }) {
+        const path = window.location.pathname !== '/' ? window.location.pathname : '';
+        window.location.href = `${path}?lang=${code}`;
     }
 }));
 
