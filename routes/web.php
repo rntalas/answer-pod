09ad1935b96e32689544d8bc243a3753851b2874
rntalas <rafailntalas@outlook.com>
2026-02-05 19:20:12 +0200

    feat(routes/web): add entry routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\EntryController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\SubjectController;
 
 Route::resource('subject', SubjectController::class);
+
+Route::resource('entry', EntryController::class);
 
 Route::get('/{slug?}', [PageController::class, 'index'])
     ->where('slug', '.*')
