0d737009ebe2ee0b904e3d8a2c56beaba1a52557
rntalas <rafailntalas@outlook.com>
2026-02-05 13:15:24 +0200

    feat: add SubjectTranslation.php

---
+++ app/Models/SubjectTranslation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SubjectTranslation extends Model
+{
+    protected $fillable = ['subject_id', 'locale_id', 'name'];
+
+    public function subject(): BelongsTo
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function locale(): BelongsTo
+    {
+        return $this->belongsTo(Locale::class);
+    }
+}
