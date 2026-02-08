febfd633ec4b81e0c396563601cce8e5256d6cc8
rntalas <rafailntalas@outlook.com>
2026-02-08 04:25:35 +0200

    chore: remove tests folder, format files

--- vite.config.js
+++ vite.config.js
@@ -14,9 +14,9 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 5173,
         strictPort: true,
-        origin: `${process.env.DDEV_PRIMARY_URL.replace(/:\d+$/, "")}:5173`,
+        origin: `${process.env.DDEV_PRIMARY_URL?.replace(/:\d+$/, "")}:5173`,
         cors: {
-            origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+            origin: /https?:\/\/([A-Za-z0-9\-]+)?(\.ddev\.site)(?::\d+)?$/,
         },
     },
 });
