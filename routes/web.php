d6c3621b6b86ea853ad61830954665f9eda018be
rntalas <rafailntalas@outlook.com>
2026-02-08 04:24:08 +0200

    feat(routes/web.php): add unit and home routes

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,17 @@
 use App\Http\Controllers\EntryController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\UnitController;
 
 Route::resource('subject', SubjectController::class);
 
+Route::get('unit/{id}', [UnitController::class, 'show'])
+    ->name('unit.show');
+
 Route::resource('entry', EntryController::class);
 
-Route::get('/{slug?}', [PageController::class, 'index'])
+Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::get('/{slug}', [PageController::class, 'index'])
     ->where('slug', '.*')
     ->name('page.show');
